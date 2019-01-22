95b9cb23b69167651e4bd5be86483e8a15a4e568
saksham rana <0blackbuck@gmail.com>
2019-01-22 18:29:03 -0500

    program 6

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -6,7 +6,24 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int val ;
+            int i, j, k;
+            Console.Write("Input number of lines:");
+            val = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= val; i++)
+            {
+                for (j = 1; j <= val - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
         }
     }
 }
